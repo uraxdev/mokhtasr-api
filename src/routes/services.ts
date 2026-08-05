a15ea581863f987636f58959d4d35f2a2b2e7a22
Seif Eldeen Essam <seifeldeenessam@icloud.com>
2026-08-05 21:40:50 +0300

    refactor(services): add `name` and `category` query param

--- src/routes/services.ts
+++ src/routes/services.ts
@@ -1,11 +1,23 @@
+import { Prisma } from '@/database/generated/client';
 import { client } from '@/database/lib/client';
 import { ServiceService } from '@/services/services';
 import type { Router } from 'express';
 
 export default (router: Router) => {
-	router.get('/services', async (_req, res, next) => {
+	router.get('/services', async (req, res, next) => {
+		const query = req.query as { name?: string; category?: string };
+		const where = {} as Prisma.ServiceWhereInput;
+
+		if (query.name) {
+			where.OR = [{ name: { path: ['en'], string_contains: query.name } }, { name: { path: ['ar'], string_contains: query.name } }];
+		}
+
+		if (query.category) {
+			where.categoryId = query.category;
+		}
+
 		try {
-			return res.status(200).json(await new ServiceService(client).list());
+			return res.status(200).json(await new ServiceService(client).list(where));
 		} catch (error) {
 			return next(error);
 		}
--- src/services/services.ts
+++ src/services/services.ts
@@ -1,10 +1,11 @@
+import { Prisma } from '@/database/generated/client';
 import { Client } from '@/database/lib/types';
 
 export class ServiceService {
 	constructor(private readonly client: Client) {}
 
-	async list() {
-		return await this.client.service.findMany({ orderBy: { createdAt: 'asc' } });
+	async list(where: Prisma.ServiceWhereInput = {}) {
+		return await this.client.service.findMany({ where, orderBy: { createdAt: 'asc' } });
 	}
 
 	async find(id: string) {
